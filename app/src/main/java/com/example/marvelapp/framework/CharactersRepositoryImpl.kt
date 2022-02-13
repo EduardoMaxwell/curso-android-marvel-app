42063da399d53fb64064f30d2d840b18fdbbe894
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2022-02-13 12:53:25 -0400

    Implementa repository na camada de framework

---
+++ app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.marvelapp.framework
+
+import androidx.paging.PagingSource
+import br.com.eduardomaxwell.core.data.repository.CharactersRemoteDataSource
+import br.com.eduardomaxwell.core.data.repository.CharactersRepository
+import br.com.eduardomaxwell.core.domain.model.Character
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import javax.inject.Inject
+
+class CharactersRepositoryImpl @Inject constructor(
+    private val remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
+) : CharactersRepository {
+
+    override fun getCharacters(query: String): PagingSource<Int, Character> {
+        return CharactersPaging()
+    }
+}
